39e3ecfd33b2443dac91e6b02f847b013c4753cb
Julie T <47932789+JulieT2319@users.noreply.github.com>
2019-12-09 16:20:24 -0700

    added questions array

---
+++ script.js
@@ -0,0 +1,26 @@
+var quizQuestions = [{
+    question = "Which of the following can be stored in an array?",
+    options = ["Objects", "Strings", "Numbers", "All of the above"],
+    answer = "All of the above"
+},
+{
+    question = "An array is enclosed in what?",
+    options = ["square brackets []", "curly brackets {}", "parentheses ()", "single quotes ''"],
+    answer = "square brackets []"
+},
+{
+    question = "What is used to indicate strict comparison?",
+    options = ["==", "===", "!=", "="],
+    answer = "==="
+},
+{
+    question = "JavaScript is a ___ language",
+    options = ["case sensitive", "case insensitive"],
+    answer = "case sensitive"
+},
+{
+    question = "What is the correct way to declare a variable ",
+    options = ["variable = 3;", "var = 3;", "var == 3;", "var === 3;"],
+    answer = "var = 3;"
+},
+]
