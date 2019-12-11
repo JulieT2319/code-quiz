9867c4e8ab1ab32305eba2c53950d591715b19e7
Julie Theriault <joules2319@gmail.com>
2019-12-11 00:45:18 -0700

    added variables needed for dom manipulation and quiz control

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 var quizQuestions = [{
-    question = "Which of the following can be stored in an array?",
-    options = ["Objects", "Strings", "Numbers", "All of the above"],
-    answer = "All of the above"
+	question = "Which of the following can be stored in an array?",
+	options =["Objects", "Strings", "Numbers", "All of the above"],
+	answer = "All of the above"
 },
 {
-    question = "An array is enclosed in what?",
-    options = ["square brackets []", "curly brackets {}", "parentheses ()", "single quotes ''"],
-    answer = "square brackets []"
+	question = "An array is enclosed in what?",
+	options =["square brackets []", "curly brackets {}", "parentheses ()", "single quotes ''"],
+	answer = "square brackets []"
 },
 {
-    question = "What is used to indicate strict comparison?",
-    options = ["==", "===", "!=", "="],
-    answer = "==="
+	question = "What is used to indicate strict comparison?",
+	options =["==", "===", "!=", "="],
+	answer = "==="
 },
 {
-    question = "JavaScript is a ___ language",
-    options = ["case sensitive", "case insensitive"],
-    answer = "case sensitive"
+	question = "JavaScript is a ___ language",
+	options =["case sensitive", "case insensitive"],
+	answer = "case sensitive"
 },
 {
-    question = "What is the correct way to declare a variable ",
-    options = ["variable = 3;", "var = 3;", "var == 3;", "var === 3;"],
-    answer = "var = 3;"
+	question = "What is the correct way to declare a variable ",
+	options =["variable = 3;", "var = 3;", "var == 3;", "var === 3;"],
+	answer = "var = 3;"
 },
 ]
+//DOM Elements
+var responseEl = document.getElementById("#responses");
+var timerEL = document.getElementById("#timer");
+var questionEl = document.getElementById("#questions");
+
+//display text
+var time = 75;
+
+//Answers
+var correctAnswer;
+var userAnswer;
