e6fe65f294c73529867d447f2dcda4fb7a809ff8
Julie T <joules2319@gmail.com>
2019-12-13 15:16:04 -0700

    addend checkAnswer function

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ var quizQuestions = [{
 var responseEl = document.getElementById("responses");
 var timerEL = document.getElementById("timer");
 var questionEl = document.getElementById("questions");
+var resultEl = document.getElementById("result");
 
 //display text
 var time = quizQuestions.length * 15;
@@ -48,12 +49,32 @@ function populate() {
 		var createButton = document.createElement("button");
 		var buttonText = document.createTextNode(quizQuestions[questionNow].options[i]);
 		createButton.appendChild(buttonText);
-		createButton.setAttribute("id", "choice-"+i);
+		createButton.setAttribute("id", "choice-" + i);
 		responseEl.appendChild(createButton);
+
 	}
-	questionEl.innerHTML = "<h1 class='question'>"+quizQuestions[questionNow].question+"</h1>"
+	questionEl.innerHTML = "<h1 class='question'>" + quizQuestions[questionNow].question + "</h1>"
 	correctAnswer = quizQuestions[questionNow].answer;
 	questionNow++
+}
+
+function checkAnswer() {
+	if (correctAnswer === userAnswer) {
+		resultEl.innerHTML = "<h1 class='correct'>That's correct!</h1>";
+	} else {
+		resultEl.innerHTML = "<h1 class='wrong'>That is not correct.</h1>";
+		time -= 15;
+		//for testing purposes
+		console.log(time);
+	}
+	function clearResult() {
+
+		setTimeout(function () {
+			resultEl.innerHTML = "";
+		}, 1500)
+	}
+	clearResult();
+	clearInterval(clearResult);
 }
 responseEl.addEventListener("click", function () {
 
